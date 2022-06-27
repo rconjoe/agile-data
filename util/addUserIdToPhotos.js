0e8d5e0cbd6104e8d7ac335a217604add03a8c5f
travis flake <tflake83@gmail.com>
2022-06-27 10:18:34 -0400

    userId added to photo

--- util/addUserIdToPhotos.js
+++ util/addUserIdToPhotos.js
@@ -1,5 +1,5 @@
 import { db } from '../firebase.js'
-import { collection, getDocs, updateDoc , doc, setDoc} from "firebase/firestore"; 
+import { collection, getDoc, updateDoc , doc, setDoc} from "firebase/firestore"; 
 
 //PULLING ALL USERIds FROM ALBUMS COLLECTION
 async function UserIdFromAlbums() {
@@ -11,28 +11,79 @@ async function UserIdFromAlbums() {
     });
 }
 // UserIdFromAlbums()
-
+let albumData = []
 async function getAlbumIdFromAlbums() {
     const querySnapshot = await getDocs(collection(db, "albums"));
     querySnapshot.forEach((doc) => {
-    console.log(doc.data().id);
-    return doc.data().id
+    // console.log(doc.data().id);
+    albumData.push(doc.data().id)
     });
 }
-// getAlbumIdFromAlbums()
+
 // const cityRef = doc(db, 'cities', 'BJ');
 // setDoc(cityRef, { capital: true }, { merge: true });
 //ADD USERID TO PHOTOS
 async function setUserIdToPhotoColl() {
-    const albumsSnapShot = await getDocs(collection(db, "albums"));
-    const photoRef = doc(db, "photos", "userId");
-    albumsSnapShot.forEach((doc => {
-        setDoc(photoRef, { merge: true });
-    }))
-    console.log(photoRef)
-    // await updateDoc(photoRef, {
-    // userId: UserIdFromAlbums
-    // });
+    let albumArr = []
+    let photoArr = []
+//50
+    for (let i = 1; i < 50; i++){
+        const photosSnapShot = await getDoc(doc(db, "photos", `${i}`));
+        const albumsSnapShot = await getDoc(doc(db, "albums", `${photosSnapShot.data().albumId}`));
+        
+        setTimeout(async () => {
+            await updateDoc(photosSnapShot.ref, {
+            userId: albumsSnapShot.data().userId
+            });
+
+        },1000)
+
+    }
+    // 
+
+    //USED TO EXTRACT ALL OF THE DATA TO ITERATE OVER    
+    //itereate only 5 times
+//         albumArr.push(doc.data())
+// ]    falbumsSnapShot((doc => { 
+
+        
+//     }))
+    // photosSnapShot.forEach((doc) => {
+    //     photoArr.push(doc.data())
+    // })
+    // // if album id === photos albumid update photo using album user.id
+    // for (let i = 1; i < 5000; i++){
+    //     const albumId = photoArr[i].albumId
+    //     if (!albumArr[albumId]) {
+    //         console.log(albumId)
+    //     }
+    //     const userId = albumArr[albumId].userId
+
+    //     const updatedPhoto = {
+    //         ...photoArr[i],
+    //         userId //when user
+    //     }
+    //     console.log(updatedPhoto)
+    //         // await updateDoc(photoRef, {
+    //         //    userId: albumArr[i].userId
+    //         // });
+
+        
+        
+    // }
 
 }
-setUserIdToPhotoColl()
+  
+ setUserIdToPhotoColl()
+
+// updateUser() {
+//         this.db.collection('yourDbCollection').doc('ifYourIdCostumized').update({
+//             age: newAgeHere
+//         })
+//             .then(function () {
+//                 console.log("Document successfully updated!");
+//             }).catch(function (error) {
+//                 console.error("Error removing document: ", error);
+
+//             });
+//     }
